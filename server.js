a4f9e4119eef8d65935abcf0079daa1caa95e1e5
Niels Koster <n.koster@portavita.eu>
2019-12-27 16:39:44 +0100

    Processing files... works!

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const
     app = require('express')(),
     http = require('http').createServer(app),
+    fs = require('fs'),
     io = require('socket.io')(http)
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     console.log('a user connected')
     console.log(socket.rooms)
+    socket.on('upload', data => {
+        socket.upload = '/tmp/' + data.filename
+        console.log(socket.upload)
+        socket.fd = fs.openSync(socket.upload, 'w')
+    })
+    socket.on('chunk', chunk => {
+        fs.appendFile(socket.upload, chunk, err => {
+            if (err) {
+              console.error(err)
+            }
+        })
+    })
 })
   
 http.listen(9999, () => {
-    console.log('listening on *:9999')
+    console.log('flextube listening on :9999')
 })
