8ae33163e31121cb73584da16750d06495992b34
Niels Koster <n.koster@portavita.eu>
2019-12-27 01:27:31 +0100

    Insert socket.io into express

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const
-    express = require('express'),
-    app = express()
-
-app.listen(9999)
+    app = require('express')(),
+    http = require('http').createServer(app),
+    io = require('socket.io')(http)
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+// app.use(.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+io.on('connection', function(socket) {
+    console.log('a user connected')
+})
+  
+http.listen(9999, function(){
+    console.log('listening on *:9999')
 })
